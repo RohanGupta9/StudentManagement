8b4f4f9a28a4c17069af06e553a26d8a54c67468
RohanGupta9 <guptarohan1743@gmail.com>
2024-12-04 20:52:44 +0530

    Your commit message here

--- src/main/java/com/project/StudentManagement/controller/UserController.java
+++ src/main/java/com/project/StudentManagement/controller/UserController.java
@@ -48,59 +48,33 @@ public class UserController {
 
     // Admin can add users with role Teacher or Student
     @PostMapping
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> addUser(@RequestBody Users user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-        String currentRole = userPrincipal.getRole();
-        if (currentRole.equals("STUDENT") || currentRole.equals("TEACHER")) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-                    .body(new ErrorResponse("Access Denied"));
-        }
         Users savedUser = userService.createUser(user);
         return ResponseEntity.ok(savedUser);
     }
 
     // Teacher can add users with role Student
     @PostMapping("/student")
-   // @PreAuthorize("hasRole('TEACHER')")
+    @PreAuthorize("hasRole('TEACHER')")
     public ResponseEntity<?> addStudent(@RequestBody Users user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-        String currentRole = userPrincipal.getRole();
-        if (currentRole.equals("STUDENT") || user.getRole().equals("TEACHER")) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-                    .body(new ErrorResponse("Access Denied"));
-        }
+
         Users savedUser = userService.createUser(user);
         return ResponseEntity.ok(savedUser);
     }
 
     // Teacher can update Student details
     @PutMapping("/student/{id}")
- //   @PreAuthorize("hasRole('TEACHER')")
+    @PreAuthorize("hasRole('TEACHER')")
     public ResponseEntity<?> updateStudent(@PathVariable Long id, @RequestBody Users userDetails) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-        String currentRole = userPrincipal.getRole();
-        if (currentRole.equals("STUDENT") ) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-                    .body(new ErrorResponse("Access Denied"));
-        }
+
         return ResponseEntity.ok(userService.updateUser(id, userDetails));
     }
 
     // Admin and Teacher can see details of any user with role Student
     @GetMapping("/{id}")
-    //@PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
     public Object getUserById(@PathVariable Long id) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-        String currentRole = userPrincipal.getRole();
-        if (currentRole.equals("STUDENT") ) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-                    .body(new ErrorResponse("Access Denied"));
-        }
         return userService.getUserById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -110,52 +84,25 @@ public class UserController {
     @PutMapping("/self/{id}")
     @PreAuthorize("hasRole('STUDENT') and #id == authentication.principal.id")
     public ResponseEntity<?> updateSelf(@PathVariable Long id, @RequestBody Users userDetails) {
-        // Retrieve the user and ensure only the user can update their details
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-//        String currentRole = userPrincipal.getRole();
-//        if (currentRole.equals("STUDENT") || userPrincipal.getId()==id) {
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-//                    .body(new ErrorResponse("You cannot update this user"));
-//        }
+
         Users user = userService.getUserById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
-
-//        user.setName(userDetails.getName());
-//        user.setPassword(userDetails.getPassword());
-//       // user.setStatus(userDetails.getStatus());
-//        user.setMobile_number(userDetails.getMobile_number());
-
         return ResponseEntity.ok(userService.updateUser2(id, userDetails));
     }
 
     // Update marksheet - Accessible by ADMIN and TEACHER
     @PutMapping("/{userId}/marksheet")
- //   @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
     public ResponseEntity<?> updateMarkSheet(@PathVariable Long userId, @RequestBody MarkSheet marksheet) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-        String currentRole = userPrincipal.getRole();
-        if (currentRole.equals("STUDENT")) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-                    .body(new ErrorResponse("You cannot update this user"));
-        }
+
         MarkSheet updatedMarkSheet = userService.updateMarkSheet(userId, marksheet);
         return ResponseEntity.ok(updatedMarkSheet);
     }
 
     // Get all users - Admin only
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<?> getAllUsers() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-        String currentRole = userPrincipal.getRole();
-        if (currentRole.equals("STUDENT")) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN) // 403 Forbidden
-                    .body(Collections.singletonList(new ErrorResponse("Access Denied")));
-        }
-
         List<Users> users= userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
@@ -193,11 +140,6 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Authorization header missing or invalid");
     }
 
-//    @PostMapping("/createToken")
-//    public ResponseEntity<String> createToken(){
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        UsersPrincipal userPrincipal = (UsersPrincipal) authentication.getPrincipal();
-//        return ResponseEntity.ok(userService.verify(userPrincipal));
-//    }
+
 }
 
